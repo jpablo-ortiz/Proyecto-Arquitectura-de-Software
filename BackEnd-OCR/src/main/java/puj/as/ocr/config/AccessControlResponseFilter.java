fe2b7a874c7356af2d7e2785ed377796a92c95df
Juan Pablo Ortiz <ortizrubio09@gmail.com>
2022-05-31 14:01:23 -0700

    Cors para no tener problemas de conexión

---
+++ BackEnd-OCR/src/main/java/puj/as/ocr/config/AccessControlResponseFilter.java
@@ -0,0 +1,31 @@
+package puj.as.ocr.config;
+
+import java.io.IOException;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.ext.Provider;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Estudiante
+ */
+@Provider
+
+public class AccessControlResponseFilter implements ContainerResponseFilter {
+
+    @Override
+    public void filter(ContainerRequestContext containerRequestContext,
+            ContainerResponseContext containerResponseContext) throws IOException {
+        containerResponseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
+        containerResponseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+        containerResponseContext.getHeaders().add("Access-Control-Allow-Headers", "Content-Type");
+        containerResponseContext.getHeaders().add("Access-Control-Max-Age", "1209600");
+    }
+}
